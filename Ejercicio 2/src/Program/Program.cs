57ec84968efd1e05fa5dea72506087c91f76150f
joacorial0109 <JOAQUIN.RIAL@correo.ucu.edu.uy>
2024-04-15 16:06:25 -0300

    .

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,10 +7,18 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand");
-            Console.WriteLine(appointmentResult);
+            // Crear una instancia de la cita
+            Appointment appointment1 = new Appointment("Juan Perex", "55782569", "5555-555-555", DateTime.Now, "Wall Street", "Armand");
 
-            string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "", "5555-555-555", DateTime.Now, "Queen Street", "");
+            // Crear una instancia del servicio de citas y programar la cita
+            string appointmentResult1 = AppointmentService.CreateAppointment(appointment1);
+            Console.WriteLine(appointmentResult1);
+
+            // Crear otra instancia de la cita
+            Appointment appointment2 = new Appointment("Ralf Manson", "", "5555-555-555", DateTime.Now, "Queen Street", "");
+
+            // Programar la segunda cita
+            string appointmentResult2 = AppointmentService.CreateAppointment(appointment2);
             Console.WriteLine(appointmentResult2);
         }
     }
