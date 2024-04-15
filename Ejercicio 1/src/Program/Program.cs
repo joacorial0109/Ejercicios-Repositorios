58dafd375a93454ac991b0626e6e5124ca29d1b1
joacorial0109 <JOAQUIN.RIAL@correo.ucu.edu.uy>
2024-04-15 16:24:26 -0300

    .

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -9,8 +9,11 @@ namespace Ucu.Poo.Expert
         {
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
-            book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
+            Shelf estanteria = new Shelf();
+            Shelf estanteria2 = new Shelf();
+            estanteria.Shelf("A","7");
+            estanteria2.Shelf("B","3");
+
 
         }
     }
